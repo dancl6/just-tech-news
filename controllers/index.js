05bf85bd5ccd449783a799bc85306940551b7832
Dan Lyons <danielclyons@yahoo.com>
2020-06-08 20:07:33 -0700

    14.1.4 update, issue with rendering page

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-
+const homeRoutes = require('./home-routes.js');
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
@@ -7,5 +7,7 @@ router.use('/api', apiRoutes);
 router.use((req, res) => {
   res.status(404).end();
 });
+
+router.use('/', homeRoutes);
 
 module.exports = router;
